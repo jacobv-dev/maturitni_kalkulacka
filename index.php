aa77639c717598fb6f2a413b30f92ef49e0a60e9
jacobv-dev <kuba.vorel@email.cz>
2022-02-10 17:07:23 +0100

    formatted

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html class="notranslate" translate="no" lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,30 +10,31 @@
 </head>
 
 <style>
-<?php include 'styly.css';?>
+  <?php include 'styly.css'; ?>
 </style>
 
 <body>
 
-              <form method="post" action="#forms">
-				
-				<div class="horni_vyber">
-				  <label><input type="radio" name="radioset" />Čeština</label>
-				  <label><input type="radio" name="radioset" />Angličtina</label>
-				  <label><input type="radio" name="radioset" />Jiný cizí jazyk</label>
-				</div>
+  <form method="post" action="#forms">
 
-                <br>
+    <div class="horni_vyber">
+      <label><input type="radio" name="radioset" />Čeština</label>
+      <label><input type="radio" name="radioset" />Angličtina</label>
+      <label><input type="radio" name="radioset" />Jiný cizí jazyk</label>
+    </div>
 
-				<div class="spodni_vyber">
-                	<label><input type="number" placeholder="Body z písemné části" name="cm" required />Písemná část</label>
-				
-                	<label><input type="number" placeholder="Body z ústní části" name="kg" required />Ústní část</label>
-				</div>
+    <br>
 
-                <br>
+    <div class="spodni_vyber">
+      <label><input type="number" placeholder="Body z písemné části" name="cm" required />Písemná část</label>
 
-                <input class="ok" type="submit" value="Spočítat" />
-            </form>
+      <label><input type="number" placeholder="Body z ústní části" name="kg" required />Ústní část</label>
+    </div>
+
+    <br>
+
+    <input class="ok" type="submit" value="Spočítat" />
+  </form>
 </body>
+
 </html>
